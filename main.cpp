bb30e73f208a30bc908ea5d7be30b03a46fd4cbb
Caerwyn13 <caerwyn13@hotmail.com>
2026-06-27 01:57:28 +0100

    Implemented matrix addition

--- main.cpp
+++ main.cpp
@@ -4,10 +4,9 @@
 using namespace std;
 
 class Matrix {
-private:
     vector<vector<double>> matrix;
 public:
-    Matrix(const int r, const int c) {
+    Matrix(const unsigned long r, const unsigned long c) {
         matrix.resize(r);
 
         for (int i = 0; i < r; i++) {
@@ -35,17 +34,43 @@ void displayMatrix(Matrix mat) {
     }
 }
 
+Matrix addMatrix(Matrix mat1, Matrix &mat2) {
+    // Ensure matrices can be added
+    if (mat1.getRows() != mat2.getRows() || mat1.getCols() != mat2.getCols()) {
+        cout << "Error: Matrices cannot be added!" << endl;
+        return {0, 0};
+    }
+
+    // Matrices can be added, so add them
+    // Resulting matrix
+    Matrix result(mat1.getRows(), mat1.getCols());
+
+    for (int r = 0; r < mat1.getRows(); r++) {
+        for (int c = 0; c < mat1.getCols(); c++) {
+            result[r][c] = mat1[r][c] + mat2[r][c];
+        }
+    }
+
+    return result;
+}
+
 int main() {
-    int rows, cols;
+    //TODO: Add user matrix inputs
+    Matrix testMatrix1(3, 3);
+    testMatrix1[0][0] = 1;
+    testMatrix1[0][1] = 2;
+    testMatrix1[0][2] = 3;
+    testMatrix1[1][0] = 4;
+    testMatrix1[1][1] = 5;
+    testMatrix1[1][2] = 6;
+    testMatrix1[2][0] = 7;
+    testMatrix1[2][1] = 8;
+    testMatrix1[2][2] = 9;
+    Matrix testMatrix2 = testMatrix1;   // Simplify testing
 
-    cout << "Enter number of rows: ";
-    cin >> rows;
-
-    cout << "Enter number of columns: ";
-    cin >> cols;
-
-    const Matrix userMatrix(rows, cols);
-    displayMatrix(userMatrix);
+    displayMatrix(testMatrix1);
+    cout << endl;
+    displayMatrix(addMatrix(testMatrix1, testMatrix2));
 
     return 0;
 }
